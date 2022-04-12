053b7781a5deb5702716ac1a8e403bf44799d08f
CelAn812 <celia.anca92@gmail.com>
2022-04-11 23:17:48 -0500

    First Commit

---
+++ src/packageDem4/Class1Dem4.java
@@ -0,0 +1,7 @@
+package packageDem4;
+
+public class Class1Dem4 {
+	public static void main(String[] args) {
+		System.out.println("Demonstration 1");
+	}
+}
---
+++ src/packageDem4/ClassDem4.java
@@ -0,0 +1,7 @@
+package packageDem4;
+
+public class ClassDem4 {
+	public static void main(String[] args) {
+		System.out.println("Demonstration 2");
+	}
+}
